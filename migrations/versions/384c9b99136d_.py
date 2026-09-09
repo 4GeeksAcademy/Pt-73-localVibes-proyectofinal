e96c781443b0a184fc5e0a8dbb33941a33410ec9
darksider050 <robertelias38@gmail.com>
2026-09-09 01:32:52 +0000

    detalles finales

---
+++ migrations/versions/384c9b99136d_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 384c9b99136d
+Revises: 833366c7a278, ab1b57945b4e, d2ec7e066941
+Create Date: 2026-09-09 01:31:39.879673
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '384c9b99136d'
+down_revision = ('833366c7a278', 'ab1b57945b4e', 'd2ec7e066941')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
