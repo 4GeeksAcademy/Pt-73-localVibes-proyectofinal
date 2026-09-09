a77439fa3abe3b8c7d94da101f5fbfa113286ba5
darksider050 <robertelias38@gmail.com>
2026-09-09 01:22:50 +0000

    union de front y back

---
+++ migrations/versions/ab1b57945b4e_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: ab1b57945b4e
+Revises: 
+Create Date: 2026-09-09 01:21:56.322701
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'ab1b57945b4e'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
